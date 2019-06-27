ee8d536a51af9961fa328ff41c10397cf525b318
Scott Perry <dev@numist.net>
2019-06-26 20:39:19 -0700

    Initial Commit

---
+++ Diffing/Diffing.h
@@ -0,0 +1,19 @@
+//
+//  Diffing.h
+//  Diffing
+//
+//  Created by numist on 2019-06-26.
+//  Copyright © 2019 numist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Diffing.
+FOUNDATION_EXPORT double DiffingVersionNumber;
+
+//! Project version string for Diffing.
+FOUNDATION_EXPORT const unsigned char DiffingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Diffing/PublicHeader.h>
+
+
