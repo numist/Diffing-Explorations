6784d00131c3e5a9c9df8eb50e9d201550c49622
Scott Perry <dev@numist.net>
2020-04-08 18:52:24 -0700

    Clean up empty file

--- Diffing/Algorithms/Diffing/Club/Frontier.swift
+++
@@ -1,7 +0,0 @@
-/*
- * The source in this file was written by Scott Perry <dev@numist.net>.
- *
- * The author has released it to the public domain.
- * Attribution is appreciated but not necessary.
- */
-
