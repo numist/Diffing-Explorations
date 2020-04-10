a9692df9430bca86b89a2076a8b852a5f409bd11
Scott Perry <dev@numist.net>
2020-04-09 20:07:29 -0700

    WTB: dead code warnings in Swift

--- DiffingTests/Data Structures/NgramTrieTests.swift
+++ DiffingTests/Data Structures/NgramTrieTests.swift
@@ -12,8 +12,6 @@ struct Foo {}
 
 class NgramTrieTests: XCTestCase {
 
-    static fileprivate func getSetNewChildNode(_ p: inout Foo) -> Foo { return p }
-    
     func testComparisons() {
         for _ in 0..<10 {
             let a = (Array("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.") + Array("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.").reversed()).map { MeasurementElement($0) }
