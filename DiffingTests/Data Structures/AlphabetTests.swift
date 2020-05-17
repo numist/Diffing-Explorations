6b946ef02bff157c4ae22cb8e4390a98d8693541
Scott Perry <dev@numist.net>
2020-05-16 20:56:57 -0700

    Write tests, find bugs

--- DiffingTests/Data Structures/AlphabetTests.swift
+++ DiffingTests/Data Structures/AlphabetTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 /*
  Normally I'm not a fan of white box testing, but it's possible for
- incorrectness in Alphabet to not affect the correctness of the diffing
+ incorrectness in _AlphabetTrie to not affect the correctness of the diffing
  algorithm's output while severely impacting performance.
 
  Unfortunately, since Dictionary's behaviour is so nondeterministic, it isn't
@@ -20,47 +20,76 @@ import XCTest
  */
 @testable import Diffing
 
-// TODO: convert to test AlphabetTrie
-//class AlphabetTests: XCTestCase {
-//    func testOffsetAfter() {
-//        let a = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
-//
-//        _withContiguousStorage(for: a) { b -> Void in
-//            let alpha = _Alphabet(b, in: 0..<b.count)
-//
-//            XCTAssertEqual(0, alpha.offset(of: 0, after: -10000))
-//
-//            XCTAssertEqual(0, alpha.offset(of: 0, after: -1))
-//            XCTAssertEqual(1, alpha.offset(of: 1, after: 0))
-//            XCTAssertEqual(2, alpha.offset(of: 2, after: 0))
-//            XCTAssertEqual(3, alpha.offset(of: 3, after: 0))
-//            XCTAssertEqual(4, alpha.offset(of: 4, after: 0))
-//
-//            XCTAssertEqual(5, alpha.offset(of: 0, after: 4))
-//            XCTAssertEqual(6, alpha.offset(of: 1, after: 4))
-//            XCTAssertEqual(7, alpha.offset(of: 2, after: 4))
-//            XCTAssertEqual(8, alpha.offset(of: 3, after: 4))
-//            XCTAssertEqual(9, alpha.offset(of: 4, after: 4))
-//
-//            XCTAssertEqual(nil, alpha.offset(of: 0, after: 9))
-//            XCTAssertEqual(nil, alpha.offset(of: 1, after: 9))
-//            XCTAssertEqual(nil, alpha.offset(of: 2, after: 9))
-//            XCTAssertEqual(nil, alpha.offset(of: 3, after: 9))
-//            XCTAssertEqual(nil, alpha.offset(of: 4, after: 9))
-//
-//            XCTAssertEqual(nil, alpha.offset(of: 0, after: 9000))
-//        }
-//    }
-//
-//    func testElementDoesntExist() {
-//        let a = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
-//
-//        _withContiguousStorage(for: a) { b -> Void in
-//            let alpha = _Alphabet(b, in: 0..<b.count)
-//
-//            XCTAssertEqual([], alpha.offsets(for:1234))
-//
-//            XCTAssertEqual(nil, alpha.offset(of: 1234, after: -1))
-//        }
-//    }
-//}
+class AlphabetTrieTests: XCTestCase {
+    func testOffsetAfter() {
+        let a = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+
+        _withContiguousStorage(for: a) { b -> Void in
+            let alpha = _AlphabetTrie(for: b, in: 0..<b.count)
+
+            XCTAssertEqual(0, alpha.offset(of: 0, after: -10000))
+
+            XCTAssertEqual(0, alpha.offset(of: 0, after: -1))
+            XCTAssertEqual(1, alpha.offset(of: 1, after: 0))
+            XCTAssertEqual(2, alpha.offset(of: 2, after: 0))
+            XCTAssertEqual(3, alpha.offset(of: 3, after: 0))
+            XCTAssertEqual(4, alpha.offset(of: 4, after: 0))
+
+            XCTAssertEqual(5, alpha.offset(of: 0, after: 4))
+            XCTAssertEqual(6, alpha.offset(of: 1, after: 4))
+            XCTAssertEqual(7, alpha.offset(of: 2, after: 4))
+            XCTAssertEqual(8, alpha.offset(of: 3, after: 4))
+            XCTAssertEqual(9, alpha.offset(of: 4, after: 4))
+
+            XCTAssertEqual(nil, alpha.offset(of: 0, after: 9))
+            XCTAssertEqual(nil, alpha.offset(of: 1, after: 9))
+            XCTAssertEqual(nil, alpha.offset(of: 2, after: 9))
+            XCTAssertEqual(nil, alpha.offset(of: 3, after: 9))
+            XCTAssertEqual(nil, alpha.offset(of: 4, after: 9))
+
+            XCTAssertEqual(nil, alpha.offset(of: 0, after: 9000))
+        }
+    }
+
+    func testNgramOffset() {
+        let a = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+
+        _withContiguousStorage(for: a) { b -> Void in
+            let alpha = _AlphabetTrie(for: b, in: 0..<b.count)
+
+            XCTAssertEqual(1, alpha.offset(ofRange: 1..<3, in: b, afterOrNear: -9000))
+            XCTAssertEqual(1, alpha.offset(ofRange: 1..<3, in: b, afterOrNear: 0))
+            XCTAssertEqual(6, alpha.offset(ofRange: 1..<3, in: b, afterOrNear: 2))
+            XCTAssertEqual(6, alpha.offset(ofRange: 1..<3, in: b, afterOrNear: 9))
+            XCTAssertEqual(6, alpha.offset(ofRange: 1..<3, in: b, afterOrNear: 9000))
+        }
+    }
+
+    func testElementDoesntExist() {
+        let a = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+
+        _withContiguousStorage(for: a) { b -> Void in
+            let alpha = _AlphabetTrie(for: b, in: 0..<b.count)
+
+            XCTAssertEqual([], alpha.offsets(for:1234))
+
+            XCTAssertEqual(nil, alpha.offset(of: 1234, after: -1))
+
+            _withContiguousStorage(for: a.reversed()) { c in
+                XCTAssertEqual(nil, alpha.offset(ofRange: 1..<3, in: c, afterOrNear: 0))
+            }
+        }
+    }
+
+    func testNgramDoesntExist() {
+        let a = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+
+        _withContiguousStorage(for: a) { b -> Void in
+            let alpha = _AlphabetTrie(for: b, in: 0..<b.count)
+
+            _withContiguousStorage(for: a.reversed()) { c in
+                XCTAssertEqual(nil, alpha.offset(ofRange: 1..<3, in: c, afterOrNear: 0))
+            }
+        }
+    }
+}
