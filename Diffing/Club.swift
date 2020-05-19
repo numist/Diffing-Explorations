da1d25665dee1377f1387a91620656374aa90de5
Scott Perry <dev@numist.net>
2020-05-19 16:18:30 -0700

    Hoist alphabet up a layer in preparation for the reintroduction of arrow diff

--- Diffing/Club.swift
+++ Diffing/Club.swift
@@ -6,8 +6,8 @@
  */
 
 func _club<E>(
-  from a: _Slice<E>,
-  to b: _Slice<E>
+  from a: _Slice<E>, alphabet optAlphaA: _AlphabetTrie<E>? = nil,
+  to b: _Slice<E>, alphabet optAlphaB: _AlphabetTrie<E>? = nil
 ) -> CollectionDifference<E>
 where E: Hashable {
   assert(a.range.count == 0 || b.range.count == 0 || (
@@ -20,9 +20,9 @@ where E: Hashable {
   let m = b.range.endIndex
   
   // Characterize remaining input
-  let trieA = _AlphabetTrie(for: a)
+  let trieA = optAlphaA ?? _AlphabetTrie(for: a)
   let alphaA = trieA.alphabet
-  let trieB = _AlphabetTrie(for: b)
+  let trieB = optAlphaB ?? _AlphabetTrie(for: b)
   let alphaB = trieB.alphabet
   let trieDepth = 2
 
@@ -397,128 +397,5 @@ fileprivate class _WorkQueue {
       }
       return true
     }
-  }
-}
-
-/* The Alphabet/Trie performs input characterization as well as efficient
- * membership testing for both individual elements as well as ranges (n-grams).
- */
-fileprivate struct _AlphabetTrie<Element> where Element: Hashable {
-  // Lazy construction requires a reference to the original collection
-  private let buf: _Slice<Element>
-
-  // The trie structure is used to encode the collection's n-grams
-  private class _TrieNode {
-    var children = Dictionary<Element, _TrieNode>()
-    var locations = Array<Int>()
-  }
-  private let root: _TrieNode
-  /* Due to laziness, a trie may not fully encode a string for any given depth.
-   * The below example is a valid construction for the string "1100":
-   *                     ┌─────────────────────┐
-   *               root: │locations: [-1,0,1,2]│
-   *                     └─────────────────────┘
-   *                                │
-   *                 "0" ┌──────────┴──────────┐ "1"
-   *                     ▼                     ▼
-   *            ┌────────────────┐    ┌─────────────────┐
-   *            │locations: [2,3]│    │locations: [0, 1]│
-   *            └────────────────┘    └─────────────────┘
-   *                     │                     │
-   *        "0" ┌────────┘        "0" ┌────────┴────────┐ "1"
-   *            ▼                     ▼                 ▼
-   *    ┌──────────────┐      ┌──────────────┐  ┌──────────────┐
-   *    │locations: [3]│      │locations: [2]│  │locations: [1]│
-   *    └──────────────┘      └──────────────┘  └──────────────┘
-   */
-
-  var alphabet: Set<Element> { Set(root.children.keys) }
-
-  init(for buf: _Slice<Element>) {
-    self.buf = buf
-
-    root = _TrieNode()
-    root.locations = Array(buf.range.startIndex..<buf.range.endIndex).map({ $0 - 1 })
-    extend(root)
-  }
-
-  // Called lazily when a node needs its children populated.
-  private func extend(_ node: _TrieNode) {
-    assert(node.children.count == 0,
-      "Children of node \(node) have already been calculated!")
-
-    for i in node.locations.map({ $0 + 1 }) {
-      func get<N>(_ n: inout N) -> N { n }
-      let child = get(&node.children[buf.base[i], default: _TrieNode()])
-      child.locations.append(i)
-      /* Functionally, the code above is equivalent to:
-       *
-       *  if let child = node.children[buf[i + 1]] {
-       *    child.locations.append(i + 1)
-       *  } else {
-       *    let child = _TrieNode()
-       *    child.locations.append(i + 1)
-       *    node.children[buf[i + 1]] = child
-       *  }
-       *
-       * but it should use ~50% fewer Dictionary lookups, much like changing:
-       *
-       *  `let tmp = f[e, default: 0]; f[e] = tmp + 1`
-       *
-       * to:
-       *
-       *  `f[e, default: 0] += 1`
-       *
-       * WTB: Unfortunately, here it's only worth a 25% improvement?
-       */
-    }
-  }
-
-  func offsets(for e: Element) -> [Int] {
-    return root.children[e]?.locations ?? []
-  }
-
-  func offset(
-    of ngram: _Slice<Element>,
-    afterOrNear loc: Int
-  ) -> Int? {
-    var node = root
-    for i in ngram.range.startIndex..<ngram.range.endIndex {
-      if node.children.count == 0 && node.locations.last! < (buf.range.count - 1) {
-        extend(node)
-      }
-      if let child = node.children[ngram.base[i]] {
-        node = child
-      } else {
-        return nil
-      }
-    }
-    assert(node.locations.count > 0, "n-gram node \(node) has no locations")
-    let end = bsearch(for: loc, in: node.locations) ?? node.locations.last!
-    // Return value should relate to the beginning of the n-gram
-    let result = end - (ngram.range.count - 1)
-    assert(buf.base[result] == ngram.base[ngram.range.startIndex],
-      "invalid calculation of n-gram match offset")
-    return result
-  }
-
-  func offset(of e: Element, after i: Int) -> Int? {
-    return bsearch(for: i, in: offsets(for: e))
-  }
-
-  // Factored binary search helper for membership testing functions
-  private func bsearch(for i: Int, in locations: [Int]) -> Int? {
-    var min = 0, max = locations.count
-    while min < max {
-      let pivot = (min + max)/2
-      let loc = locations[pivot]
-      if loc > i {
-        max = pivot
-      } else {
-        min = pivot + 1
-      }
-    }
-    assert(min == max, "binary search failed to resolve")
-    return min < locations.count ? locations[min] : nil
   }
 }
