e8bb766e2168bd9661e29efb2eed70d88d23128e
Scott Perry <dev@numist.net>
2020-05-19 19:45:32 -0700

    Reindent tests, improve time checking

--- DiffingTests/Algorithms/Diffing/DiffingTestCase.swift
+++ DiffingTests/Algorithms/Diffing/DiffingTestCase.swift
@@ -11,72 +11,99 @@ import Diffing
 let printStats = true
 
 class DiffingTestCase: XCTestCase {
+  
+  private static var intro: () = {
+    print("        ┌───────────────────────────────────────────────────────────┐")
+    print("        │                          Legend:                          │")
+    print("        │                                                           │")
+    print("        │ 'n=' -- size of the collections being diffed              │")
+    print("        │ '==' -- count of element equality evaluations             │")
+    print("        │'|Δ|' -- count of individual changes in the diffs          │")
+    print("        │  '⌛︎' -- wall clock time (when both algorithms exceed 10ms)│")
+    print("        └───────────────────────────────────────────────────────────┘")
+  }()
+  
+  override static func setUp() {
+    super.setUp()
+    _ = intro
+  }
+  
+  override func setUp() {
+    super.setUp()
     
-    private static var intro: () = {
-        print("================================================================================")
-        print("\tWelcome! Here's a legend for the performance test output:")
-        print("\t\t \"n=\": the size of the collections being diffed")
-        print("\t\t \"==\": the number of element equality evaluations")
-        print("\t\t\"|Δ|\": the number of changes in the diff produced by the algorithm")
-        print("\t\t  \"⌛︎\": wall clock time (only when both algorithms exceed 10ms)")
-    }()
+    if printStats {
+      print("================================= hybrid/myers =================================")
+    }
+  }
+  
+  override func tearDown() {
+    if printStats {
+      print("================================================================================")
+    }
+    
+    super.tearDown()
+  }
+  
+  func measureDiffs<C, D>(from old: C, to new: D, strict: Bool = false)
+    where C : BidirectionalCollection, D : BidirectionalCollection,
+    C.Element == D.Element, C.Element : Hashable
+  {
+    let a = old.map { MeasurementElement($0) }
+    let b = new.map { MeasurementElement($0) }
+    
+    let measureCount = 5
+    
+    comparisons = 0
+    var myersStart = Date()
+    let md = difference(from: a, to: b, using: ==)
+    var myersElapsed = -myersStart.timeIntervalSinceNow
+    let baseline = comparisons
+    
+    if strict && myersElapsed < 0.2 {
+      myersStart = Date()
+      for _ in 0..<measureCount {
+        _ = difference(from: a, to: b, using: ==)
+      }
+      myersElapsed = -myersStart.timeIntervalSinceNow / Double(measureCount)
+    }
+    
+    comparisons = 0
+    var hybridStart = Date()
+    let hd = difference(from: a, to: b)
+    var hybridElapsed = -hybridStart.timeIntervalSinceNow
+    let hybrid = comparisons
+    
+    if strict && hybridElapsed < 0.2 {
+      hybridStart = Date()
+      for _ in 0..<measureCount {
+        _ = difference(from: a, to: b)
+      }
+      hybridElapsed = -hybridStart.timeIntervalSinceNow / Double(measureCount)
+    }
 
-    override func setUp() {
-        super.setUp()
-        
-        if printStats {
-            _ = DiffingTestCase.intro
-            print("================================= hybrid/myers =================================")
-        }
-    }
     
-    override func tearDown() {
-        if printStats {
-            print("================================================================================")
-        }
-
-        super.tearDown()
+    let ratio = Double(hybrid)/Double(baseline)
+    if printStats {
+      print("n=(\(a.count),\(b.count)):", terminator: "\t")
+      print("==:\(hybrid)/\(baseline) (\(String(format: "%.01f",ratio*100.0))%)", terminator: "\t")
+      print("|Δ|:\(hd.count)/\(md.count) (+\(String(format: "%.01f",hd.count==md.count ? 0.0 : Double(hd.count)/Double(md.count)*100.0-100.0))%)", terminator: "\t")
+      print("⌛︎:\(String(format: "%.02f",hybridElapsed))/\(String(format: "%.02f",myersElapsed))", terminator: "")
+      if myersElapsed > 0.001 && hybridElapsed > 0.001 {
+        print(" (\(String(format: "%.01f",(hybridElapsed/myersElapsed)*100.0))%)", terminator: "")
+      }
+      print("")
     }
-    
-    func measureDiffs<C, D>(from old: C, to new: D, strict: Bool = false)
-        where C : BidirectionalCollection, D : BidirectionalCollection,
-        C.Element == D.Element, C.Element : Hashable
-    {
-        let a = old.map { MeasurementElement($0) }
-        let b = new.map { MeasurementElement($0) }
-        
-        comparisons = 0
-        let myersStart = Date()
-        let md = difference(from: a, to: b, using: ==)
-        let myersElapsed = -myersStart.timeIntervalSinceNow
-        let baseline = comparisons
-      
-        comparisons = 0
-        let hybridStart = Date()
-        let hd = difference(from: a, to: b)
-        let hybridElapsed = -hybridStart.timeIntervalSinceNow
-        let hybrid = comparisons
-        
-        let ratio = Double(hybrid)/Double(baseline)
-        if printStats {
-            print("n=(\(a.count),\(b.count)):", terminator: "\t")
-            print("==:\(hybrid)/\(baseline) (\(String(format: "%.01f",ratio*100.0))%)", terminator: "\t")
-            print("|Δ|:\(hd.count)/\(md.count) (+\(String(format: "%.01f",hd.count==md.count ? 0.0 : Double(hd.count)/Double(md.count)*100.0-100.0))%)", terminator: "\t")
-            if myersElapsed > 0.01 && hybridElapsed > 0.01 {
-                print("⌛︎:\(String(format: "%.02f",hybridElapsed))/\(String(format: "%.02f",myersElapsed)) (\(String(format: "%.01f",(hybridElapsed/myersElapsed)*100.0))%)", terminator: "\t")
-            }
-            print("")
-        }
-        XCTAssert(a.applying(hd) == b)
-        if a.count + b.count > 500 && md.count > 50 {
-            XCTAssertLessThan(ratio, 4.0)
-        }
-        if myersElapsed > 0.01 && hybridElapsed > 0.01 {
-          XCTAssertLessThanOrEqual(hybridElapsed, myersElapsed * 2)
-        }
-        if strict {
-            XCTAssertLessThanOrEqual(hd.count, md.count * 2)
-        }
+    XCTAssert(a.applying(hd) == b)
+    if a.count + b.count > 500 && md.count > 50 {
+      XCTAssertLessThan(ratio, 4.0)
     }
+    // `strict` is only set to true for all unit test cases that represent common real-world workloads
+    if strict {
+      XCTAssertLessThanOrEqual(hd.count, md.count * 2)
+      if myersElapsed > 0.016 && hybridElapsed > 0.016 {
+        XCTAssertLessThanOrEqual(hybridElapsed, myersElapsed * 1.5)
+      }
+    }
+  }
 }
-// ::==--
+
--- DiffingTests/Algorithms/Diffing/LiteralTests.swift
+++ DiffingTests/Algorithms/Diffing/LiteralTests.swift
@@ -8,52 +8,59 @@
 import Foundation
 
 class LiteralTests: DiffingTestCase {
+  // All literal tests are strictly validated
+  func measureDiffs<C, D>(from old: C, to new: D)
+    where C : BidirectionalCollection, D : BidirectionalCollection,
+    C.Element == D.Element, C.Element : Hashable
+  {
+    measureDiffs(from: old, to: new, strict: true)
+  }
 
-    func testHamlin() {
-        let a = Array("call this so we can override.")
-        let b = Array("so we can override")
-        measureDiffs(from: a, to: b, strict: true)
-    }
+  func testHamlin() {
+    let a = Array("call this so we can override.")
+    let b = Array("so we can override")
+    measureDiffs(from: a, to: b)
+  }
 
-    func testLoremIpsums() {
-        let a = Array("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-        let b = Array("Loremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non-numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laborum.")
-        measureDiffs(from: a, to: b, strict: true)
-    }
+  func testLoremIpsums() {
+    let a = Array("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    let b = Array("Loremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non-numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laborum.")
+    measureDiffs(from: a, to: b)
+  }
 
-    func testLoremIpsumsByWord() {
-        let a = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".split(separator: " ")
-        let b = "Loremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non-numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laborum.".split(separator: " ")
-        measureDiffs(from: a, to: b, strict: true)
-    }
+  func testLoremIpsumsByWord() {
+    let a = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".split(separator: " ")
+    let b = "Loremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non-numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laborum.".split(separator: " ")
+    measureDiffs(from: a, to: b)
+  }
 
-    func testMuspiMerol() {
-        let a = Array("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-        let b = a.reversed()
-        measureDiffs(from: a, to: b, strict: true)
-    }
+  func testMuspiMerol() {
+    let a = Array("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    let b = a.reversed()
+    measureDiffs(from: a, to: b)
+  }
+  
+  func testReversed() {
+    let a = Array(0..<1000)
+    let b = a.reversed()
+    measureDiffs(from: a, to: b)
+  }
+  
+  func testBtree79ce96ab39Tof55ea8f456ByLine() {
+    let testBundle = Bundle(for: LiteralTests.self)
     
-    func testReversed() {
-        let a = Array(0..<1000)
-        let b = a.reversed()
-        measureDiffs(from: a, to: b, strict: true)
-    }
+    let url79ce96ab39 = testBundle.url(forResource: "btree.79ce96ab39", withExtension: "c")!
+    let btree79ce96ab39 = try! String(contentsOf: url79ce96ab39, encoding: .utf8).split(separator: "\n", omittingEmptySubsequences: false)
     
-    func testBtree79ce96ab39Tof55ea8f456ByLine() {
-        let testBundle = Bundle(for: LiteralTests.self)
-        
-        let url79ce96ab39 = testBundle.url(forResource: "btree.79ce96ab39", withExtension: "c")!
-        let btree79ce96ab39 = try! String(contentsOf: url79ce96ab39, encoding: .utf8).split(separator: "\n", omittingEmptySubsequences: false)
-        
-        let urlf55ea8f456 = testBundle.url(forResource: "btree.f55ea8f456", withExtension: "c")!
-        let btreef55ea8f456 = try! String(contentsOf: urlf55ea8f456, encoding: .utf8).split(separator: "\n", omittingEmptySubsequences: false)
-        
-        measureDiffs(from: btree79ce96ab39, to: btreef55ea8f456, strict: true)
-    }
+    let urlf55ea8f456 = testBundle.url(forResource: "btree.f55ea8f456", withExtension: "c")!
+    let btreef55ea8f456 = try! String(contentsOf: urlf55ea8f456, encoding: .utf8).split(separator: "\n", omittingEmptySubsequences: false)
     
-    func testSame() {
-        let a = Array("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-        let b = a
-        measureDiffs(from: a, to: b, strict: true)
-    }
+    measureDiffs(from: btree79ce96ab39, to: btreef55ea8f456)
+  }
+  
+  func testSame() {
+    let a = Array("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    let b = a
+    measureDiffs(from: a, to: b)
+  }
 }
